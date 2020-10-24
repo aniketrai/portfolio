8c2d5a5d989eb4dcce5e88ef9635f98a3aa0d09b
Aniket Rai <aniket.rai@hach.com>
2020-10-24 23:09:48 +0530

    Changed font familty

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { fromEvent, Observable, Subscription } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, startWith, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  desktopWidth = 1024;
+
+  isDesktop: boolean;
+  resizeSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+    this.resizeSubscription = fromEvent(window, 'resize')
+      .pipe(
+        debounceTime(200),
+        map(() => window.innerWidth), //Don't use mapTo! 
+        distinctUntilChanged(),
+        startWith(window.innerWidth),
+        tap(x => x > this.desktopWidth ? this.isDesktop = true : this.isDesktop = false),
+        tap(x => console.log(this.isDesktop))
+      ).subscribe();
   }
 
+  ngOnDestroy() {
+    this.resizeSubscription.unsubscribe();
+  }
 }
